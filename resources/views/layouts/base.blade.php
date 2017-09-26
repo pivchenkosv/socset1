9507a9ddd4a290e8dcba68adc7a7162690b78593
Yan <kadron141@gmail.com>
2017-09-26 14:29:05 +0300

    gh

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" >
+        <meta charset="utf-8">
+        <title>socset</title>
+    </head>
+    <body>
+        <div class = "header"> Header </div>
+        
+        <div class= "content">
+         @yield('content')
+        </div>
+        <div class = "footer"> Copyright rzhpiv </div>
+    </body>
+</html>
