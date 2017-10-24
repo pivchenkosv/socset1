f41de37ab941a07358648431860ffa03031dc1d5
Yan <kadron141@gmail.com>
2017-10-24 14:55:57 +0300

    24.10.2017, 14:55:57,93

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Catalog;
-
-class ServicesController extends Controller
+use App\Products;
+class ProductsController extends Controller
 {
     public function getCatalog($id = 0)
     {
-        $catalog = Catalog::find($id);
         $products = Product::where('catalog_id',$id) -> get();
-        return view('catalog', compact('catalog','products'));
+        return view('products', compact('products'));
     }
     public function getAll()
     {
         $catalogs = Catalog::all();
-        return view('catalogs', compact('catalogs')); 
+        return view('products', compact('products')); 
     }
 }
